cae0a197d8f246a1a71b0cf7779fb17344fbce5a
tebecheri10 <tebecheri10@gmail.com>
2023-05-27 19:03:12 +0200

    title added to the home view

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,6 +4,7 @@ import MarketingApp from './components/MarketingApp'
 const App = ()=>{
     return (
         <div>
+            <h1>Welcome to the Microfrontend App</h1>
             <MarketingApp/>
         </div>
     )
